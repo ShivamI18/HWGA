f313f7bd55bdb397c2325e200b48d9b1268bc262
ShivamI18 <148793975+ShivamI18@users.noreply.github.com>
2025-03-14 20:10:45 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,11 +12,23 @@ let package = Package(
             targets: ["BackgroundVideoRecorder"]
         )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.15.0"),
+        .package(url: "https://github.com/BradLarson/GPUImage.git", from: "0.1.7")
+    ],
     targets: [
         .target(
             name: "BackgroundVideoRecorder",
-            dependencies: [],
+            dependencies: [
+                "Alamofire",
+                "SwiftyJSON",
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
+                "GPUImage"
+            ],
             path: "BackgroundVideoRecorder"
         )
     ]
